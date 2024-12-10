191126b558087be1c1e049532608eaa0da69ba0a
Sobirjon005 <sobirjon0305@gmail.com>
2024-12-10 12:49:49 +0500

    Fast Api

--- 01-project/05-task.py
+++ 01-project/05-task.py
@@ -24,7 +24,7 @@ async def create_book(book: BookSchema):
     books.append(book)
     print(books)
     print(book.json())
-    return books
+    return book
 
 
 @app.get("/book/{book_id}")
@@ -32,4 +32,13 @@ async def read_book():
     return books
 
 
+@app.put("/book/{book_id}")
+async def update_book(book_id: int, updated_book: BookSchema):
+    books[book_id] = updated_book
+    return updated_book.model_dump()
 
+
+@app.delete("/book/{book_id}")
+async def delete_book(book_id: int):
+    del books[book_id]
+    return status.HTTP_204_NO_CONTENT
